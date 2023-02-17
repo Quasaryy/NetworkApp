d13bda8d1a42b973bfbbca856b9770745bafc726
Yury <quasaryy@gmail.com>
2023-02-17 23:01:33 +0400

    Optional type for "var users" changed to non-optional

--- NetworkApp/Models/Model.swift
+++ NetworkApp/Models/Model.swift
@@ -7,39 +7,23 @@
 
 import Foundation
 
-// MARK: - Welcome
+// MARK: - User
 struct User: Decodable {
     let results: [Result]
 }
 
 // MARK: - Result
 struct Result: Decodable {
-    let gender: String
     let name: Name
     let email: String
-    let login: Login
     let dob, registered: Dob
-    let phone, cell: String
-    let id: ID
     let picture: Picture
-    let nat: String
 }
 
 // MARK: - Dob
 struct Dob: Decodable {
     let date: String
     let age: Int
-}
-
-// MARK: - ID
-struct ID: Decodable {
-    let name: String
-}
-
-// MARK: - Login
-struct Login: Decodable {
-    let uuid, username, password, salt: String
-    let md5, sha1, sha256: String
 }
 
 // MARK: - Name
